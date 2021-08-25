08a98dfff4fcc18d01c9c6202bfe678001229b5c
Eundaa <eundada064@gmail.com>
2021-08-25 22:02:24 +0900

    [2021.08.25] 백준 2739풀이 등록

---
+++ Baekjoon/2739.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('/dev/stdin').toString();
+let number = Number(input[0]);
+for(let i = 1; i <= 9; i++){
+    console.log(number +" * " + i + " = " + (number*i));
+}
